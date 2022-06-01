fd953abb4403314cc831f7627becfa6ee1da68d0
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2022-06-01 21:24:58 +0300

    Исправлен баг в тестовом файле 'GenderExistence'

--- src/test/java/dev/kalenchukov/fieldvaluevalidator/supports/existences/GenderExistence.java
+++ src/test/java/dev/kalenchukov/fieldvaluevalidator/supports/existences/GenderExistence.java
@@ -9,11 +9,15 @@ package dev.kalenchukov.fieldvaluevalidator.supports.existences;
 import dev.kalenchukov.fieldvaluevalidator.interfaces.Existable;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
 public class GenderExistence implements Existable<String>
 {
 	@Override
 	public boolean exists(@NotNull String value)
 	{
+		Objects.requireNonNull(value);
+
 		return value.equals("MALE") || value.equals("FEMALE");
 	}
 }
